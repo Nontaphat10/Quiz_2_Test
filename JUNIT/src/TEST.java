dd6947e8e7f466c888d5c8b6233e82b21fc2decd
Nontaphat10 <Nontaphat8562@gmail.com>
2023-12-18 11:17:48 +0700

    1

---
+++ JUNIT/src/TEST.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TEST {
+	@Before
+	public void init() {
+		obj = new QUIZ();
+	}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
